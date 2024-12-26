5f27e9cf5f975564e20148ede3f6f216544de61c
ayushFple <ayush@fple.in>
2024-12-27 01:00:43 +0530

    Minor Changes

--- Service/SearchComponent.test.tsx
+++ Service/SearchComponent.test.tsx
@@ -3,21 +3,18 @@ import {render, fireEvent} from '@testing-library/react-native';
 import SearchComponent from '../Components/SearchComponent';
 
 test('renders correctly and handles input', () => {
-  const mockSetSearchQuery = jest.fn(); // Mock the setSearchQuery function
+  const mockSetSearchQuery = jest.fn();
 
   const {getByPlaceholderText, getByText} = render(
-    <SearchComponent setSearchQuery={mockSetSearchQuery} />, // Pass the mocked function as a prop
+    <SearchComponent setSearchQuery={mockSetSearchQuery} />,
   );
 
   const input = getByPlaceholderText('Enter login');
   const button = getByText('Submit');
 
-  // Simulate typing in the input field
   fireEvent.changeText(input, 'johndoe');
 
-  // Simulate pressing the submit button
   fireEvent.press(button);
 
-  // Assert that the mocked function was called with the correct argument
   expect(mockSetSearchQuery).toHaveBeenCalledWith('johndoe');
 });
